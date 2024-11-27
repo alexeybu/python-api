d0a11acf64078d18c37b57ec9fa83494a3013e7b
Alexey Burich <alekseiburich@gmail.com>
2024-11-27 14:20:10 +0400

    Add files via upload

---
+++ ticker_info_api.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+def get_recommendation(tck):
+    rec = tck.recommendations_summary
+    if rec.shape[0] > 0:
+        rec.drop(columns=['period'], inplace=True)
+        rec.columns = ['Strong Buy', 'Buy', 'Hold', 'Sell', 'Strong Sell']
+        rec_perc = rec.div(rec.sum(axis=1), axis=0)
+        
+        rec_perc['max_value'] = rec_perc.max(axis=1)
+        rec_perc['max_column'] = rec_perc.idxmax(axis=1)
+    
+        return rec_perc.iloc[0]['max_column'], rec_perc.iloc[0]['max_value']
+    else:
+        return '-', 0
+
+def get_ticker_information(ticker):
+    tck = yf.Ticker(ticker)
+    recommendation, recommendation_percent = get_recommendation(tck)
+    if (len(tck.calendar['Earnings Date']) != 0):
+        earnings_date = str(tck.calendar['Earnings Date'][0]) 
+    else:
+        earnings_date = '-'
+    targets = tck.analyst_price_targets
+    price_low = targets['low']
+    price_high = targets['high']
+    price_mean = targets['mean']
+    return {"recommendation": recommendation, 
+            "recommendation_percent": np.round(recommendation_percent*100, 0),
+            "earnings_date": earnings_date, 
+            "price_low": price_low,
+            "price_high": price_high,
+            "price_mean": price_mean}
+app = FastAPI()
+
+@app.get("/{ticker}")
+def root(ticker: str):
+	return get_ticker_information(ticker)
